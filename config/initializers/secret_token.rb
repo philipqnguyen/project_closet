3cbb5ade692c7851e412c111ed530e55a16bbbbc
Philip Nguyen <supertaru@gmail.com>
2014-05-02 15:27:39 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectCloset::Application.config.secret_key_base = '5853f1a767aeb7e4f5e14397bb4dbc7d1210136b861c0cbd1caaba8e8a21a9c24b8d399493711460ed5ca5ef4d16d5d15010594954702b01693729271576af2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectCloset::Application.config.session_store :cookie_store, key: '_project_closet_session'
